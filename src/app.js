79df7bf8e6d62b8e692737834725deea24ca989f
sofiareyes12 <121201sofiareyes@gmail.com>
2025-09-17 21:18:39 +0200

    config and db_connedctions modified

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
 import ReminderRoutes from "./routes/ReminderRoutes.js";
+import UserRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
 
 const app = express();
+
+// Middlewares
 app.use(bodyParser.json());
+app.use(express.json());
 
 // Rutas
 app.use("/reminders", ReminderRoutes);
+app.use("/api/users", UserRoutes);
+
+// Middleware de errores simple
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({ error: err.message || "Internal Server Error" });
+});
 
 // Puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-// Rutas
-app.use('/api/users', require('./routes/userRoutes'));
-
-// Middleware de errores simple
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
-});
-
-module.exports = app;
+export default app;
